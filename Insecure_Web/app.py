f505d65a959d60e3b1f5e245c5398a4a3aa40d00
AYS <mintchoco701@gmail.com>
2022-04-20 00:56:07 +0900

    modify login & add board

--- Insecure_Web/app.py
+++ Insecure_Web/app.py
@@ -1,10 +1,11 @@
 from datetime import timedelta
 import sys,os
+from tkinter import EXCEPTION
 from flask import Flask, render_template, redirect, url_for, request, flash, session
 import sqlite3
 
 
- # DB 연결
+ # DB  연결
 def dbcon():
     return sqlite3.connect(os.path.dirname(os.path.realpath(__file__))+'/mydb.db')
 
@@ -15,9 +16,21 @@ def create_db():
         print ("Opened database successfully")
         c = conn.cursor()
         c.execute('CREATE TABLE IF NOT EXISTS member (id varchar(50) PRIMARY KEY NOT NULL, passwd varchar(50) NOT NULL, nickname varchar(50) NOT NULL)')
-        print ("Table created successfully")
+        print ("'member' Table created successfully")
+        c.execute('CREATE TABLE IF NOT EXISTS board (num INTEGER PRIMARY KEY AUTOINCREMENT,id TEXT NOT NULL,title TEXT NOT NULL, content TEXT, reg_date DEFAULT CURRENT_TIMESTAMP NOT NULL)')
+        print ("'board' Table created successfully")
         c.execute("INSERT INTO member VALUES ( 'admin', 'admin','admin')")
         print ("Insert Admin")
+        c.execute("INSERT INTO board (id,title,content) VALUES ('HELLO','THIS_IS','TEST_CONTENT')")
+        c.execute("INSERT INTO board (id,title,content) VALUES ('HELLO','THIS_IS','TEST_CONTENT')")
+        c.execute("INSERT INTO board (id,title,content) VALUES ('HELLO','THIS_IS','TEST_CONTENT')")
+        c.execute("INSERT INTO board (id,title,content) VALUES ('HELLO','THIS_IS','TEST_CONTENT')")
+        c.execute("INSERT INTO board (id,title,content) VALUES ('HELLO','THIS_IS','TEST_CONTENT')")
+        c.execute("INSERT INTO board (id,title,content) VALUES ('HELLO','THIS_IS','TEST_CONTENT')")
+        c.execute("INSERT INTO board (id,title,content) VALUES ('HELLO','THIS_IS','TEST_CONTENT')")
+        c.execute("INSERT INTO board (id,title,content) VALUES ('HELLO','THIS_IS','TEST_CONTENT')")
+        c.execute("INSERT INTO board (id,title,content) VALUES ('Java','script','<script>alert(1);</script>')")
+        print("Insert Test data")
         conn.commit()
     except Exception as e:
         print('db error: ',e)
@@ -70,41 +83,69 @@ def select_id(id):
     finally:
         db.close()
         return ret
- 
+
+# 로그인 시 id, passsword 확인 함수
 def check_passwd(id,input_passwd):
+    print("check_passwd call")
     try:
         db = dbcon()
         c = db.cursor()
-        query = "SELECT passwd FROM member where id = '" + id + "'"
-        query = f"SELECT nickname from member WHERE id='{id}' AND passwd = '{input_passwd}';"
+        query = f"SELECT id, passwd, nickname from member WHERE id='{id}' AND passwd = '{input_passwd}';"
         c.execute(query)
+        print("execute")
+        
+        rows = c.fetchall()
+        print("fetchall")
+        print("입력한 id = ",id,"입력한 password = ",input_passwd)
+        # 쿼리 결과문이 리스트로 나오기에 for문
+        for rs in rows:
+            print(id,'의 비밀번호는 ', rs[1],'입니다')
+            if id == rs[0] and input_passwd == rs[1]:
+                print("login Success")
+                return True
+            else:
+                print("login failed")
+                return False
     except Exception as e:
         print('db error [check_passwd()] : ',e)
-    else:
-        # 쿼리 결과문이 리스트로 나옴
-        if c.fetchall():
-            print("Password Correct!")
-            return True
-        else:
-            print("Password Incorrect")
+
+# 글 읽기
+def read_post(num):
+    print("read_post call")
+    try:
+        db = dbcon()
+        c = db.cursor()
+        query = f"SELECT * FROM board WHERE num = {num};"
+        c.execute(query)
+        rows = c.fetchone()
+        print("해당 글 ",rows)
+        c.close()
+        if rows == None:
             return False
+        return rows
+    except Exception as e:
+        print('db error [read_post()] : ',e)
+
 
 app = Flask(__name__)
 app.secret_key = 'this is secret key'
 # 세션 지속 시간 10분
 app.config["PERMANET_SESSION_LIFETIME"] = timedelta(minutes=10)
 
+# index page
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
+# 로그아웃 구현
 @app.route('/logout')
 def logout():
     # logout and redirect ro index page
-    session.pop("username",None)
+    session.pop("userid",None)
+    flash("로그아웃 완료")
     return redirect(url_for('index'))
 
-
+# 로그인 후 첫 페이지
 @app.route('/homepage')
 def homepage():
     # 게시판 등 로그아웃 구현
@@ -114,6 +155,7 @@ def homepage():
     return render_template('homepage.html', userid=userid)
 
 
+# 로그인 화면
 @app.route('/login', methods=['GET','POST'])
 def login():
     if request.method == 'POST':
@@ -129,6 +171,7 @@ def login():
         return render_template('login.html')
     return render_template('login.html')
 
+# 회원가입 화면
 @app.route('/register',methods=['GET','POST'])
 def register():
     error = None
@@ -149,8 +192,55 @@ def register():
 
     return render_template('register.html', error=error)
 
+# board page
+@app.route('/board',methods=['GET'])
+def board():
+    error = None
+    # 세션으로 로그인 유저 아이디 저장
+    id = session['userid']
+    conn = dbcon()
+    cursor = conn.cursor()
+    sql = "SELECT * FROM board ORDER BY reg_date desc"
+    cursor.execute(sql)
+    data = cursor.fetchall()
+    
+    data_list = []
+    # id, title, content, reg_data
+    for obj in data:
+        data_dic = {
+            'num' : obj[0],
+            'id' : obj[1],
+            'title' : obj[2],
+            'content' : obj[3],
+            'time' : obj[4]
+        }
+        data_list.append(data_dic)
+    cursor.close()
+    conn.close()
+    
+    return render_template('board.html', error=error, name=id, data_list= data_list)
+    
+@app.route('/board_view/<int:num>')
+def board_vew(num):
+    post = read_post(num)
+    if post == False:
+        flash("해당 글은 존재하지 않습니다!")
+        return redirect(url_for('board'))
+    data_dic = {
+        'num' : post[0],
+        'id' : post[1],
+        'title' : post[2],
+        'content' : post[3],
+        'time' : post[4]
+    }
+    print(post)
+    print(post[3])
+    print(type(post[3]))
+    return render_template('post_view.html',data = data_dic)
+    
+
 if __name__ == "__main__":
-    dbcon()
+    #dbcon()
     create_db()
     app.run()
    
